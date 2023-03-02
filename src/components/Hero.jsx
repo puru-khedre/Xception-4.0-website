9fc17905a289879590cb7f5f78b8f1f389b0f7fb
Purushottam Khedre <purushottamkhedre123@gmail.com>
2023-03-02 16:25:49 +0530

    refactor

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const Hero = () => {
   return (
-    <div className="h-max border border-emerald-400 bg-emerald-200 relative">
+    <div className="h-max border relative">
       <img src="/images/bg.png" className="w-full " />
       <Navbar className="absolute" />
     </div>
