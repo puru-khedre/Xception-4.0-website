aed0ef96a7066cd2f6f9524b2c3d10c518fe346b
puru-khedre <101047627+puru-khedre@users.noreply.github.com>
2023-03-03 10:04:30 +0000

    responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,46 +1,74 @@
 import React from "react";
-import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare } from "react-icons/fa";
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaTwitterSquare,
+} from "react-icons/fa";
 import SocialLinks from "./SocialLinks";
 
 const Footer = () => {
   return (
     <div className="w-full py-3 backdrop-filter backdrop-blur-sm bg-opacity-60 bg-theme-light shadow-md">
-      <div className="w-9/12 mx-auto flex flex-row text-theme-xdark font-navbar text-lg justify-between">
-        <div className="w-1/2">
+      <div className="w-9/12 mx-auto flex flex-col gap-2 md:gap-0 md:flex-row text-theme-xdark font-navbar text-lg justify-between">
+        <div className="w-full md:w-1/2">
           <h2 className="font-semibold text-theme-dark text-2xl">
             Xception 4.0, 2023
           </h2>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus dolor maiores culpa corrupti accusantium consectetur eaque mollitia repellendus neque! Accusantium</p>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis, ducimus animi. Cupiditate perferendis rerum veritatis?</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
+            dolor maiores culpa corrupti accusantium consectetur eaque mollitia
+            repellendus neque! Accusantium
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis,
+            ducimus animi. Cupiditate perferendis rerum veritatis?
+          </p>
         </div>
-        <div className="flex flex-col gap-2">
-          <h3 className="text-navbar text-2xl text-theme-dark">Social links</h3>
-          <SocialLinks icon={<FaFacebookSquare
-          e className="text-theme-dark text-xl" />} name="Facebook" />
-          <SocialLinks icon={<FaInstagramSquare className="text-theme-dark text-xl"/>} name="Instagram" />
-          <SocialLinks icon={<FaTwitterSquare className="text-theme-dark text-xl"/>} name="Instagram" />
-        </div>
-        <div className="flex flex-col gap-2">
-          <a className="hover:underline" href="/">
-            Home
-          </a>
-          <a className="hover:underline" href="/sponcers">
-            Sponcers
-          </a>
-          <a className="hover:underline" href="/events">
-            Events
-          </a>
-          <a className="hover:underline" href="/merchandise">
-            Merchandise
-          </a>
-          <a className="hover:underline" href="/aboutUs">
-            AboutUs
-          </a>
+        <div className="w-full md:w-1/2 flex flex-col md:flex-row">
+          <div className="flex flex-col gap-2">
+            <h3 className="text-navbar text-2xl text-theme-dark">
+              Social links
+            </h3>
+            <SocialLinks
+              icon={<FaFacebookSquare e className="text-theme-dark text-xl" />}
+              name="Facebook"
+            />
+            <SocialLinks
+              icon={<FaInstagramSquare className="text-theme-dark text-xl" />}
+              name="Instagram"
+            />
+            <SocialLinks
+              icon={<FaTwitterSquare className="text-theme-dark text-xl" />}
+              name="Instagram"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <h3 className="text-navbar text-2xl text-theme-dark">
+              Important links
+            </h3>
+            <a className="hover:underline" href="/">
+              Home
+            </a>
+            <a className="hover:underline" href="/sponcers">
+              Sponcers
+            </a>
+            <a className="hover:underline" href="/events">
+              Events
+            </a>
+            <a className="hover:underline" href="/merchandise">
+              Merchandise
+            </a>
+            <a className="hover:underline" href="/aboutUs">
+              AboutUs
+            </a>
+          </div>
         </div>
       </div>
-      <img src="/images/line.svg" className="rotate-180 object-cover w-10/12 mx-auto my-2"/>
+      <img
+        src="/images/line.svg"
+        className="rotate-180 object-cover w-10/12 mx-auto my-2"
+      />
     </div>
-
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,15 @@ import NavLinks from "./NavLinks";
 
 const Navbar = ({ className }) => {
   const scrollPosition = useScrollPosition();
+  const [navigationOpen, setIsNavigationOpen] = false;
   return (
     <div
-      className={`${className} font-navbar p-11 z-50 text-[#290505] flex flex-row items-center justify-between w-full transition-all ${
+      className={`${className} font-navbar p-11 z-40 text-[#290505] flex flex-row items-center justify-between w-full transition-all ${
         scrollPosition > 0 &&
         "py-3 backdrop-filter backdrop-blur-lg bg-opacity-30 bg-[#633d2c] shadow-md"
       }`}
     >
-      <div className="text-5xl font-semibold">
+      <div className="text-2xl md:text-5xl font-semibold">
         <h1>Xception 4.0</h1>
       </div>
       <div className="flex flex-row items-center gap-4 text-2xl">
@@ -20,6 +21,7 @@ const Navbar = ({ className }) => {
         <NavLinks name={"Merchandise"} />
         <NavLinks name={"About Us"} />
       </div>
+      <div className="absolute w-screen h-screen backdrop-filter backdrop-blur-lg bg-opacity-30 inset-0"></div>
     </div>
   );
 };
